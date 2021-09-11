374b330dda5b3139bb4dd49fc640247dcf048cab
ki-elijah <elijahknsubuga@gmail.com>
2021-09-11 22:44:26 +0300

    first commit

---
+++ gdrive.py
@@ -0,0 +1,14 @@
+from pydrive.drive import GoogleDrive
+from pydrive.auth import GoogleAuth
+
+gat = GoogleAuth()
+gat.LocalWebServerAuth()
+
+dr = GoogleDrive(gat)
+path = "/home/elabz/Documents/Books"
+
+for x in os.listdir(path):
+    y = drive.CreateFile({title: x})
+    y.SetContentFile(os.path.join(path, x))
+    y.Upload()
+    y = NoneType
